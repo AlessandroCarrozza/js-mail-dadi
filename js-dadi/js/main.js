668ea4ce4a57548593b330e833350b71920e79fe
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-02-27 15:08:02 +0100

    Progetto completato

--- js-dadi/js/main.js
+++ js-dadi/js/main.js
@@ -1,9 +1,11 @@
 
-
+// creo collegamento con il DOM
 const resultDom = document.getElementById("result");
 
+// assegno alle due variabili un numero casuale da 1 a 6
 let numUser = Math.floor(Math.random() * 6) + 1;
 let numPc = Math.floor(Math.random() * 6) + 1;
+
 
 if (numUser > numPc) {
     resultDom.innerHTML = `L'utente ha vinto per ${numUser} a ${numPc}.`;
