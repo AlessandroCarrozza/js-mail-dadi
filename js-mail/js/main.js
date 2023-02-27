4dc3bb2724e6ccd2ebcbc3dadda08a800610cf0c
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-02-27 15:00:23 +0100

    Istruzioni per la console

--- js-mail/js/main.js
+++ js-mail/js/main.js
@@ -8,6 +8,11 @@ const emailUser = prompt("Inserisci qui la tua email");
 
 for (let i = 0; i < emailsList.length; i++) {
     
-    
+    if (emailUser == emailsList[i]) {
+        console.log("email corretta");
+        break;
+    } else if (i == (emailsList.length -1)) {
+        console.log("email errata");
+    }
     
 }
