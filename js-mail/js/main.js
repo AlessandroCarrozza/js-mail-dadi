4cc01feb032e5b71597e3ae63193093b24325e9d
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-02-27 14:41:47 +0100

    Inizializzazione

---
+++ js-mail/js/main.js
@@ -0,0 +1,13 @@
+
+// lista delle email registrate
+const emailsList = ["pippopluto@gmail.com", "mariorossi@gmail.com", "marcobianchi@gmail.com"];
+
+
+// chiedo all'utente la sua email
+const emailUser = prompt("Inserisci qui la tua email");
+
+for (let i = 0; i < emailsList.length; i++) {
+    
+    
+    
+}
